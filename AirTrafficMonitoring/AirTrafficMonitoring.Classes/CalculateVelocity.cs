deaf820952bfbf8e3d9c9519ec9dc9bf94d7c733
Soeren Bech <soeren-bechy7@hotmail.com>
2018-04-17 15:10:08 +0200

    Redigerede Caltulaters og lavede test for CalculateVelocity

---
+++ AirTrafficMonitoring/AirTrafficMonitoring.Classes/CalculateVelocity.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AirTrafficMonitoring.Classes
+{
+    public class CalculateVelocity
+    {
+        public int Velocity(int oldX, int newX, int oldY, int newY)
+        {
+            int x = Math.Abs(newX - oldX);
+            int y = Math.Abs(newY - oldY);
+
+            return (int)Math.Sqrt(x*x  + y*y);
+        }
+    }
+}
