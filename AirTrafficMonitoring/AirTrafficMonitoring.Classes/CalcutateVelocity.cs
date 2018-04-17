2fbb2dff7a41d46e3997fe6f0e428a88d9e24707
Soeren Bech <soeren-bechy7@hotmail.com>
2018-04-17 10:54:11 +0200

    Lavede Calculates, samt test for CalculateCourse

---
+++ AirTrafficMonitoring/AirTrafficMonitoring.Classes/CalcutateVelocity.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AirTrafficMonitoring.Classes
+{
+    class CalcutateVelocity
+    {
+        public double Velocity(string oldX, string newX, string oldY, string newY)
+        {
+            double x = Math.Abs(int.Parse(newX) - int.Parse(oldX));
+            double y = Math.Abs(int.Parse(newY) - int.Parse(oldY));
+
+            return Math.Sqrt(x * x + y * y);
+        }
+    }
+}
